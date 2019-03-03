37a90efcc81424c8bb439dcc9edef7e7bca69395
foxman <lin.vladimir@gmail.com>
2019-03-03 13:01:26 +0800

    add compile scripts

--- CiosNations.bat
+++ CiosNations.bat
@@ -1,8 +1,23 @@
 @echo off
 
-set LIBRARIES=^
-libexpat
+set NAME=libnations
+set CIOSNATIONS=%~dp0
+set ORIGINAL=%CD%
+set COMPILEPATH=%CD%\Nations
+set /p VERSION=<%CIOSNATIONS%\VERSION.txt
 
-for %%i in (%LIBRARIES%) do (
-  @call %~dp0\%%i\%%i.bat
-)
+mkdir %COMPILEPATH%
+cd %COMPILEPATH%
+
+cmd /A /Q /K %CIOSNATIONS%\scripts\x86.bat %COMPILEPATH% Release static 1 0
+cmd /A /Q /K %CIOSNATIONS%\scripts\x86.bat %COMPILEPATH% Release shared 0 1
+cmd /A /Q /K %CIOSNATIONS%\scripts\x86.bat %COMPILEPATH% Debug   static 1 0
+cmd /A /Q /K %CIOSNATIONS%\scripts\x86.bat %COMPILEPATH% Debug   shared 0 1
+cmd /A /Q /K %CIOSNATIONS%\scripts\x64.bat %COMPILEPATH% Release static 1 0
+cmd /A /Q /K %CIOSNATIONS%\scripts\x64.bat %COMPILEPATH% Release shared 0 1
+cmd /A /Q /K %CIOSNATIONS%\scripts\x64.bat %COMPILEPATH% Debug   static 1 0
+cmd /A /Q /K %CIOSNATIONS%\scripts\x64.bat %COMPILEPATH% Debug   shared 0 1
+
+cd %ORIGINAL%
+rd /S /Q %COMPILEPATH%
+echo Compile Done
